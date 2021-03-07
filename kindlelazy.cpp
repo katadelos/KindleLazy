74dd4785b3744cba7baea28d19f19a169b90ea00
katadelos <katadelos@protonmail.com>
2021-03-07 18:45:05 +0000

    Clean up kindlelazy.cpp

--- kindlelazy.cpp
+++ kindlelazy.cpp
@@ -1,24 +1,22 @@
 /*
-KindleLazy 0.9
-cearp 2016
+KindleLazy 0.9.1
+Copyright 2016 <cearp>
 
 thanks to lucida for usbotgmouse and kindle-evdev
 thanks to knc1 and everyone else at the mobileread kindle developer's corner
 */
 
+#include <fcntl.h>
+#include <sys/poll.h>
+#include <unistd.h>
+#include <linux/input.h>
+#include <X11/extensions/XTest.h>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <fcntl.h>
 #include <fstream>
 #include <memory>
-#include <sys/poll.h>
-#include <unistd.h>
 #include <vector>
-
-#include <linux/input.h>
-#include <X11/extensions/XTest.h>
-
 #include "json/json.h"
 
 enum action_type {
@@ -41,7 +39,7 @@ int halfheight = 0;
 int halfwidth = 0;
 bool reverse = false;
 
-//touch screen device file
+// touch screen device file
 int eventfdTOUCH;
 
 std::string new_brightness = "";
@@ -65,35 +63,36 @@ std::string exec(const char* cmd) {
     return result;
 }
 
-int getbtn(int fd)
-{
+int getbtn(int fd) {
     int btn = 0;
     struct input_event t;
-    if (read(fd, &t, sizeof(t)) == sizeof(t)){
-        if (t.type == EV_KEY && t.value == 0)  //key released
+    if (read(fd, &t, sizeof(t)) == sizeof(t)) {
+        if (t.type == EV_KEY && t.value == 0)  // key released
             btn = t.code;
-        else if (t.type == EV_REL && t.code == REL_WHEEL && t.value == 1)  //vscroll up
+        else if (t.type == EV_REL && t.code == REL_WHEEL && t.value == 1)
+            // vscroll up
             btn = 9001;
-        else if (t.type == EV_REL && t.code == REL_WHEEL && t.value == -1)  //vscroll down
+        else if (t.type == EV_REL && t.code == REL_WHEEL && t.value == -1)
+            // vscroll down
             btn = 9002;
-        else if (t.type == EV_REL && t.code == REL_HWHEEL && t.value == -1)  //hscroll left
+        else if (t.type == EV_REL && t.code == REL_HWHEEL && t.value == -1)
+            // hscroll left
             btn = 9003;
-        else if (t.type == EV_REL && t.code == REL_HWHEEL && t.value == 1)  //hscroll right
+        else if (t.type == EV_REL && t.code == REL_HWHEEL && t.value == 1)
+            // hscroll right
             btn = 9004;
     }
     return btn;
 }
 
-void mouse_click(Display* dpy, int x, int y)
-{
-    XTestFakeMotionEvent (dpy, -1, x, y, CurrentTime);
-    XTestFakeButtonEvent (dpy, 1, True,  CurrentTime);
-    XTestFakeButtonEvent (dpy, 1, False, CurrentTime);
+void mouse_click(Display* dpy, int x, int y) {
+    XTestFakeMotionEvent(dpy, -1, x, y, CurrentTime);
+    XTestFakeButtonEvent(dpy, 1, True,  CurrentTime);
+    XTestFakeButtonEvent(dpy, 1, False, CurrentTime);
     XFlush(dpy);
 }
 
-void write_event(int fd, __u16 type, __u16 code, __s32 value)
-{
+void write_event(int fd, __u16 type, __u16 code, __s32 value) {
     int ret;
     struct input_event event;
     memset(&event, 0, sizeof(event));
@@ -106,9 +105,8 @@ void write_event(int fd, __u16 type, __u16 code, __s32 value)
     fsync(fd);
 }
 
-void write_touch_event(int fd, int x, int y)
-{
-    //For kindle, get sequence from "evtest /dev/input/eventX"
+void write_touch_event(int fd, int x, int y) {
+    // For kindle, get sequence from "evtest /dev/input/eventX"
     write_event(fd, EV_KEY, BTN_TOUCH, 1);
     write_event(fd, EV_ABS, ABS_MT_TRACKING_ID, 0);
     write_event(fd, EV_ABS, ABS_MT_POSITION_X , x);
@@ -123,58 +121,54 @@ void write_touch_event(int fd, int x, int y)
     write_event(fd, EV_SYN, 0, 0);
 }
 
-void process_key (action_type action)
-{
-    //when faking a touch event and not just faking a simple mouse click
-    //the orientation is ignored, and there is NOT a x,y point that turns/reverses for both portrait and lanscape
-    //that turns/reverse the page for both portrait and landscape
-    //we could check the orientation via 'lipc-get-prop com.lab126.winmgr orientation'
+void process_key(action_type action) {
+    /*  when faking a touch event and not just faking a simple mouse click the
+        orientation is ignored, and there is NOT a x,y point that turns/reverse
+        the page for both portrait and landscape we could check the orientation
+        via 'lipc-get-prop com.lab126.winmgr orientation'
+    */
 
-    //i have no idea about oasis orientation values
+    // i have no idea about oasis orientation values
     std::string orientation = exec("lipc-get-prop com.lab126.winmgr orientation");
-    orientation.pop_back(); //it retuns with a newline... took a while to figure why compares weren't working
-    if(orientation.compare("U") == 0){
-        //upright/portrait
+    orientation.pop_back();
+    if (orientation.compare("U") == 0) {
+        // upright/portrait
         next_x = width;
         next_y = halfheight;
         prev_x = 0;
         prev_y = halfheight;
-    }
-    else{
-        //landscape
+    } else {
+        // landscape
         next_x = halfwidth;
         next_y = 0;
         prev_x = halfwidth;
         prev_y = height;
     }
 
-    switch(action)
-    {
+    switch (action) {
         case key_next_page:
-        if(reverse){
+        if (reverse) {
             // mouse_click(dpy, 0, halfheight);
             write_touch_event(eventfdTOUCH, prev_x, prev_y);
-        }
-        else{
+        } else {
             // mouse_click(dpy, width, halfheight);
             write_touch_event(eventfdTOUCH, next_x, next_y);
         }
         break;
 
         case key_prev_page:
-        if(reverse){
+        if (reverse) {
             // mouse_click(dpy, width, halfheight);
             write_touch_event(eventfdTOUCH, next_x, next_y);
-        }
-        else{
+        } else {
             // mouse_click(dpy, 0, halfheight);
             write_touch_event(eventfdTOUCH, prev_x, prev_y);
         }
         break;
 
         case key_brightness_up:
-        current_brightness = std::stoi( exec("lipc-get-prop com.lab126.powerd flIntensity"));
-        if(current_brightness < 24){
+        current_brightness = std::stoi(exec("lipc-get-prop com.lab126.powerd flIntensity"));
+        if (current_brightness < 24) {
             current_brightness++;
         }
         new_brightness = std::to_string(current_brightness);
@@ -184,7 +178,7 @@ void process_key (action_type action)
 
         case key_brightness_down:
         current_brightness = std::stoi(exec("lipc-get-prop com.lab126.powerd flIntensity"));
-        if(current_brightness > 0){
+        if (current_brightness > 0) {
             current_brightness--;
         }
         new_brightness = std::to_string(current_brightness);
@@ -194,74 +188,71 @@ void process_key (action_type action)
 
         case key_wake:
         std::string state = exec("lipc-get-prop com.lab126.powerd state");
-        state.pop_back(); //remove newline from string
-        if(state.compare("screenSaver") == 0){
+        state.pop_back();  // remove newline from string
+        if (state.compare("screenSaver") == 0) {
             exec("lipc-set-prop com.lab126.powerd wakeUp 1");
         }
         break;
     }
 
-    //keep awake. at least faking touch with xdotool, we needed this.
-    //otherwise the kindle sleeps due to no touch - we probably need it here too.
+    // keep awake. at least faking touch with xdotool, we needed this. otherwise
+    // the kindle sleeps due to no touch - we probably need it here too.
     system("lipc-set-prop com.lab126.powerd -i touchScreenSaverTimeout 1");
-
 }
 
-int main (int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     std::vector<keycommand> the_keys;
 
-    printf("KindleLazy v0.9 - cearp\n\n");
+    printf("KindleLazy v0.9.1 - cearp\n\n");
 
     std::ifstream ifs("config.json");
     Json::Reader reader;
     Json::Value obj;
-    if(!reader.parse(ifs, obj)){
+    if (!reader.parse(ifs, obj)) {
         printf("Problem with config.json - check it exists and is valid!\n");
         printf("Exiting\n");
         return -1;
     }
+    const Json::Value& json_next_page = obj["key_next_page"];
+    const Json::Value& json_prev_page = obj["key_prev_page"];
+    const Json::Value& json_brightness_up = obj["key_brightness_up"];
+    const Json::Value& json_brightness_down = obj["key_brightness_down"];
+    const Json::Value& json_wake = obj["key_wake"];
 
-    //argc is the int that stores how many arguments. always 1 or more? 1 because the filename is the first argument (argv[0])
-    if (argc >= 2)
-    {
-        if (std::string(argv[1]) == "-reverse")
-        { 
+    // argc is the int that stores how many arguments. always 1 or more?
+    // 1 because the filename is the first argument (argv[0])
+    if (argc >= 2) {
+        if (std::string(argv[1]) == "-reverse") {
             reverse = true;
             printf("Page direction is reversed\n");
         }
     }
 
-    const Json::Value& json_next_page = obj["key_next_page"];
-    for (int i = 0; i < json_next_page.size(); i++){
+    for (int i = 0; i < json_next_page.size(); i++) {
         keycommand temp;
         temp.id = json_next_page[i].asInt();
         temp.action = key_next_page;
         the_keys.push_back(temp);
     }
-    const Json::Value& json_prev_page = obj["key_prev_page"];
-    for (int i = 0; i < json_prev_page.size(); i++){
+    for (int i = 0; i < json_prev_page.size(); i++) {
         keycommand temp;
         temp.id = json_prev_page[i].asInt();
         temp.action = key_prev_page;
         the_keys.push_back(temp);
     }
-    const Json::Value& json_brightness_up = obj["key_brightness_up"];
-    for (int i = 0; i < json_brightness_up.size(); i++){
+    for (int i = 0; i < json_brightness_up.size(); i++) {
         keycommand temp;
         temp.id = json_brightness_up[i].asInt();
         temp.action = key_brightness_up;
         the_keys.push_back(temp);
     }
-    const Json::Value& json_brightness_down = obj["key_brightness_down"];
-    for (int i = 0; i < json_brightness_down.size(); i++){;
+    for (int i = 0; i < json_brightness_down.size(); i++) {;
         keycommand temp;
         temp.id = json_brightness_down[i].asInt();
         temp.action = key_brightness_down;
         the_keys.push_back(temp);
     }
-    const Json::Value& json_wake = obj["key_wake"];
-    for (int i = 0; i < json_wake.size(); i++){;
+    for (int i = 0; i < json_wake.size(); i++) {;
         keycommand temp;
         temp.id = json_wake[i].asInt();
         temp.action = key_wake;
@@ -278,53 +269,53 @@ int main (int argc, char* argv[])
 
     int btn = 0;
 
-
     eventfdTOUCH = open("/dev/input/event1", O_WRONLY);
-    if (eventfdTOUCH < 0) //failed
-    {   
+    if (eventfdTOUCH < 0) {
+        // failed
         perror("ERROR opening evdev1 (touch screen) device\n");
-    }
-    else{
+    } else {
         printf("opened evdev1 touch screen\n");
     }
 
     eventfd2 = open("/dev/input/event2", O_RDONLY);
-    if (eventfd2 < 0) //failed
-    {	
+    if (eventfd2 < 0) {
+        // failed
         perror("ERROR opening evdev2 device\n");
-    }
-    else{
+    } else {
         printf("opened event2\n");
     }
 
     eventfd3 = open("/dev/input/event3", O_RDONLY);
-    if (eventfd3 < 0) //failed
-    {   
+    if (eventfd3 < 0) {
+        // failed
         perror("ERROR opening evdev3 device\n");
-    }
-    else{
+    } else {
         printf("opened event3\n");
     }
 
 
     dpy = XOpenDisplay(NULL);
-    if ( dpy == NULL) {
+    if (dpy == NULL) {
         perror("ERROR Opening XDisplay\n");
         return -1;
     }
 
-    //get screen res so we know where to touch
-    //if in book view, in portrait/landscape, X will report the correct respective resolution.
-    //Although 9% of the time KindleLazy is loaded in portrait (via KUAL) - if run from ssh whilst in landscape book view, the res will be landscape and be a problem.
-    //when we write a touch to /dev/input/event1 (touch screen), X's rotation/book rotation does not matter, coordinates are ALWAYS in 'portrait'
-    //so, just incase KindleLazy starts up in landscape, we make sure that height < width, if not, swap them.
+    /*  get screen res so we know where to touch
+
+        If in book view, in portrait/landscape, X will report the correct
+        respective resolution. Although 9% of the time KindleLazy is loaded in
+        portrait (via KUAL) - if run from ssh whilst in landscape book view,
+        the res will be landscape and be a problem. When we write a touch to
+        /dev/input/event1 (touch screen), X's rotation/book rotation does not
+        matter, coordinates are ALWAYS in 'portrait'. So, just in case
+        KindleLazy starts up in landscape, we make sure that height < width, if
+        not, swap them.
+    */
     Screen*  scrn = DefaultScreenOfDisplay(dpy);
-    if(scrn->height > scrn->width){
+    if (scrn->height > scrn->width) {
         height = scrn->height;
         width = scrn->width;
-    }
-    else
-    {
+    } else {
         width = scrn->height;
         height = scrn->width;
     }
@@ -337,44 +328,40 @@ int main (int argc, char* argv[])
     mypoll3.fd = eventfd3;
     mypoll3.events = POLLIN;
 
-    while(1) {
-        if(poll(&mypoll2, 1, 10) > 0) {
+    while (1) {
+        if (poll(&mypoll2, 1, 10) > 0) {
             btn = getbtn(mypoll2.fd);
-            if (btn <= 0){
+            if (btn <= 0) {
                 continue;
             }
 
-            //i'm not sure how inefficient it is to cycle through all configured keys to see if the current input matches
-            //but we are dealing with a small amount of data so it shouldn't matter here :)
-            for (const keycommand& i : the_keys)
-            {
-                if (btn == i.id)
-                {
+            /*  I'm not sure how inefficient it is to cycle through all
+                configured keys to see if the current input matches but we are
+                dealing with a small amount of data so it shouldn't matter here
+                :)
+            */
+            for (const keycommand& i : the_keys) {
+                if (btn == i.id) {
                     process_key(i.action);
                 }
             }
-
         }
-        if(poll(&mypoll3, 1, 10) > 0) {
+        if (poll(&mypoll3, 1, 10) > 0) {
             btn = getbtn(mypoll3.fd);
-            if (btn <= 0){
+            if (btn <= 0) {
                 continue;
             }
 
-            for (const keycommand& i : the_keys)
-            {
-                if (btn == i.id)
-                {
+            for (const keycommand& i : the_keys) {
+                if (btn == i.id) {
                     process_key(i.action);
                 }
             }
-
         }
-
     }
- 
 
-    XCloseDisplay (dpy);
+
+    XCloseDisplay(dpy);
 
     close(eventfd2);
     close(eventfd3);
